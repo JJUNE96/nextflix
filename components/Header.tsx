99c1cbd4c8ead4ced76a6e2becda04138428cb14
jjune96 <hj09060906@gmail.com>
2023-03-15 10:03:05 +0900

    setting

---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+	return (
+		<header>
+			<h1>june</h1>
+			<ul className='bg-[aqua] text-[#fff]'>
+				<li className='text-[#fff]'>menu</li>
+				<li>menu</li>
+				<li>menu</li>
+				<li>menu</li>
+				<li>menu</li>
+				<li>menu</li>
+			</ul>
+		</header>
+	);
+}
+
+export default Header;
