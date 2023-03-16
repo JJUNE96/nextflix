a2f50d08197634b3e1c852602112de72f258b164
jjune96 <hj09060906@gmail.com>
2023-03-16 14:33:58 +0900

    login하면 배너 설정

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,31 +2,32 @@ import Head from 'next/head';
 
 function login() {
 	return (
-		<div className='relative flex h-screen flex-col md:items-center md:justify-center'>
+		<div className='relative flex h-screen flex-col  md:items-center md:justify-center'>
 			<Head>
-				<title>Nextflix Member</title>
-				<link rel='icon' href='favicon.icon' />
+				<title>Netflix Member</title>
+				<link rel='icon' href='favicon.ico' />
 			</Head>
 
 			<img
 				src='https://rb.gy/ulxxee'
 				alt='netflix'
-				width={200}
+				width={260}
 				height={130}
-				className='absolute left-4 top-4 object-contain cursor-pointer md:left10 md:top-6 md:mt-0 md:max-w-md d:px-14'
+				className='absolute left-4 top-4 object-contain cursor-pointer md:left10 md:top-6 md:mt-0 md:max-w-md md:px-14'
 			/>
 
 			<form className='relative mt-24 space-y-8 rounded bg-black/70 py-10 px-6 md:max-w-md md:px-14'>
-				<h1>Sign In</h1>
-				<div>
-					<input type='email' placeholder='Email' />
-					<input type='password' placeholder='Password' />
+				<h1 className='text-4xl font-semibold'>Sign In</h1>
+
+				<div className='space-y-4'>
+					<input type='email' placeholder='Email' className='input' />
+					<input type='password' placeholder='Password' className='input' />
 				</div>
 
-				<button>Sign In</button>
+				<button className='w-full rounded bg-[red] py-3 font-semibold'>Sign In</button>
 
-				<div>
-					New to Netflix? <button>Sign Up Now</button>
+				<div className='text-[gray]'>
+					New to Netflix? <button className='text-white ml-4 hover:underline'>Sign Up Now</button>
 				</div>
 			</form>
 		</div>
